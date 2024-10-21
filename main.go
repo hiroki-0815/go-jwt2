fcc8febead5cd04b663f64f0d4a2452bbb7514ac
hiroki <haohuirixia851@gmail.com>
2024-10-21 16:21:25 +0900

    connect to db

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	initializers "github.com/hiroki-0815/go-jwt2/initializer"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
 
 func main() {
-	fmt.Println("Hello2")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
