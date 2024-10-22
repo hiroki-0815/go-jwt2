9be08ad8d3b8029c214a676aaf6bcff1139a2361
hiroki <haohuirixia851@gmail.com>
2024-10-22 11:33:30 +0900

    build login func

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	initializers "github.com/hiroki-0815/go-jwt2/initializer"
 	"github.com/hiroki-0815/go-jwt2/models"
 	"golang.org/x/crypto/bcrypt"
@@ -46,4 +49,63 @@ func Signup(c *gin.Context) {
 
 	// Respond
 	c.JSON(http.StatusOK, gin.H{})
+}
+
+func Login(c *gin.Context) {
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	// Look up requested user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Ivailid email or password",
+		})
+		return
+	}
+
+	// Compare sent in pass with saved user hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid eamil or password",
+		})
+		return
+	}
+
+	// Generate a jwt token
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token",
+		})
+		return
+	}
+
+	// send it back
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{})
+
 }
